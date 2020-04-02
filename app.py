781fa1911403dcf48532e99863971bbfeb295af3
terry <n4tlt.radio@gmail.com>
2020-04-02 15:09:47 -0400

    This is the main training project for VS. I still have much more to do.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_bootstrap import Bootstrap
 from typing import List, Tuple
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
+import paramiko
 import yaml
 import os
 
@@ -23,9 +24,14 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 app.config['SECRET_KEY'] = os.urandom(24)
 mysql = MySQL(app)
 
+# Configure paramiko
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 host_list: Tuple = ()
-host_name = None
+user_list: Tuple = ()
+host_name = ""
+message = ""
 
 
 @app.route('/')
@@ -37,12 +43,13 @@ def home() -> str:
 def login():
     if request.method == "POST":
         global host_list
+        global user_list
         try:
             form = request.form
             login_id = form["login_id"]
             password = form["password"]
             cursor = mysql.connection.cursor()
-            query = f"SELECT password FROM user WHERE login_id = '{login_id}'" \
+            query = f"SELECT * FROM user WHERE login_id = '{login_id}'" \
                     f" AND password = '{password}'"
             result = cursor.execute(query)
             print(query)
@@ -51,8 +58,10 @@ def login():
                 return render_template('login.html')
             else:
                 print("**************LOADING ACTIONS*****************")
+                user_list = cursor.fetchall()
                 cursor.close()
                 host_list = get_database_data()
+                print(host_list)
                 return render_template('actions.html', hosts=host_list)
         except mysql.connection.Error as err:
             print(err)
@@ -62,12 +71,16 @@ def login():
         return render_template('login.html')
 
 
-@app.route("/foo", methods=["POST"])
-def foo():
+@app.route("/redirect", methods=["GET", "POST"])
+def redirect():
+    global message
     global host_name
-    host_name = request.form.get("host_name")
-    print(f"******************{host_name}************************")
-    return render_template('actions.html', hosts=host_list)
+    if request.method == "POST":
+        host_name = request.form.get("host_name")
+        message = f"Executing commands on {host_name}..."
+        print(f"***{message}***")
+
+    return render_template('actions.html', hosts=host_list, message=message)
 
 
 @app.route('/actions', methods=["GET", "POST"])
@@ -82,54 +95,81 @@ def actions(hosts):
 
 @app.route('/disk_usage')
 def disk_usage():
-    data = subprocess.Popen(["./calvin.sh", "df"], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = data.communicate()
-    if stderr:
-        raise Exception("Error "+str(stderr))
-    data_str = stdout.decode('utf-8')
-    print(data_str)
-    return render_template('disk_usage.html', output=data_str)
+    try:
+        connect_host()
+    except:
+        exception_str = "Authentication Error!"
+        return render_template('ifconfig.html', output=exception_str)
+
+    stdin, stdout, stderr = ssh_client.exec_command("df -H")
+    retstr = f"HOST: {host_name}\n{format_output(stdout, stderr)}"
+    return render_template('disk_usage.html', output=retstr)
 
 
 @app.route('/processes')
 def processes():
-    data = subprocess.Popen(["./calvin.sh", "ps"], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = data.communicate()
-    if stderr:
-        raise Exception("Error "+str(stderr))
-    data_str = stdout.decode('utf-8')
-    print(data_str)
-    return render_template('processes.html', output=data_str)
+    try:
+        connect_host()
+    except:
+        exception_str = "Authentication Error!"
+        return render_template('ifconfig.html', output=exception_str)
+
+    stdin, stdout, stderr = ssh_client.exec_command("ps uwax")
+    retstr = f"HOST: {host_name}\n{format_output(stdout, stderr)}"
+    return render_template('processes.html', output=retstr)
 
 
 @app.route('/netstat_i')
 def netstat_i():
-    return render_template('netstat_i.html')
+    try:
+        connect_host()
+    except:
+        exception_str = "Authentication Error!"
+        return render_template('ifconfig.html', output=exception_str)
+
+    stdin, stdout, stderr = ssh_client.exec_command("netstat -i")
+    retstr = f"HOST: {host_name}\n{format_output(stdout, stderr)}"
+    return render_template('netstat_i.html', output=retstr)
 
 
 @app.route('/netstat_r')
 def netstat_r():
-    return render_template('netstat_r.html')
+    try:
+        connect_host()
+    except:
+        exception_str = "Authentication Error!"
+        return render_template('ifconfig.html', output=exception_str)
+
+    stdin, stdout, stderr = ssh_client.exec_command("netstat -r")
+    retstr = f"HOST: {host_name}\n{format_output(stdout, stderr)}"
+    return render_template('netstat_r.html', output=retstr)
 
 
 @app.route('/ifconfig')
 def ifconfig():
-#   value = request.form.get['vm_host']
-#   print(f"******************{value}************************")
+    try:
+        connect_host()
+    except:
+        exception_str = "Authentication Error!"
+        return render_template('ifconfig.html', output=exception_str)
 
-    print(f"************************{host_name}****************************")
-    data = subprocess.Popen(["./calvin.sh", "ifconfig"], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = data.communicate()
-    if stderr:
-        raise Exception("Error "+str(stderr))
-    data_str = stdout.decode('utf-8')
-    print(data_str)
-    return render_template('ifconfig.html', output=data_str)
+    stdin, stdout, stderr = ssh_client.exec_command("ifconfig")
+    retstr = f"HOST: {host_name}\n{format_output(stdout, stderr)}"
+    return render_template('ifconfig.html', output=retstr)
 
 
 @app.route('/passwd')
 def passwd():
-    return render_template('passwd.html')
+    try:
+        connect_host()
+    except:
+        exception_str = "Authentication Error!"
+        return render_template('ifconfig.html', output=exception_str)
+
+    stdin, stdout, stderr = ssh_client.exec_command("cat /etc/passwd")
+
+    retstr = f"HOST: {host_name}\n{format_output(stdout, stderr)}"
+    return render_template('passwd.html', output=retstr)
 
 
 @app.route("/about")
@@ -139,7 +179,7 @@ def about():
 
 def get_database_data():
     cursor = mysql.connection.cursor()
-    query = "SELECT host_name FROM vm_data"
+    query = "SELECT * FROM vm_data"
     print(query)
     result = cursor.execute(query)
     if result > 0:
@@ -147,6 +187,32 @@ def get_database_data():
 
     return None
 
+
+def format_output(output, error):
+    error_list = error.readlines()
+    output_list = output.readlines()
+    retstr = ""
+
+    if error_list:
+        for line in error_list:
+            retstr = retstr + line
+    else:
+        for line in output_list:
+            retstr = retstr + line
+    return retstr
+
+
+def connect_host():
+    ip_address: str = ""
+    for data in host_list:
+        if data.get("host_name") == host_name:
+            ip_address = data.get("ip_address")
+            break
+
+    ssh_client.connect(
+        hostname=ip_address,
+        username=user_list[0].get('login_id'),
+        password=user_list[0].get('password'))
 
 
 if __name__ == '__main__':
